1789c5568afa04c9aa8c59d5d4d1e417eed6e9a6
Ye0ngJae <kimjh38011@gmail.com>
2022-12-04 17:16:15 +0900

    main

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        userid = request.form.get("userid")
+        password = request.form.get("password")
+        return userid + password
+    
+        return "<script>alert('Wrong id/pw');history.back(-1);</script>";
