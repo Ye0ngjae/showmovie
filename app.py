2f4f737887c15593ff2acffbfa9e9faf80688d2e
Ye0ngJae <kimjh38011@gmail.com>
2022-12-11 18:38:33 +0900

    update

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ def admin():
     if request.method == 'GET':
         return render_template('request.html')
     else:
-        print(request.form.get('url'))
+        print('POST')
         
-
 if __name__ == '__main__':
     app.run(debug=True)
--- movie.py
+++ movie.py
@@ -1,7 +1,61 @@
 import requests 
+import urllib.request
 from bs4 import BeautifulSoup
 import time
+import os
+from lxml import etree
 
 url = 'https://movie.naver.com/'
 
-r = requests.get(url).text
+def get_num(link):
+    num = link.split('=')[1]
+    save_img(num)
+    
+    return num
+
+def save_img(num):
+    img_url = url+'movie/bi/mi/photoViewPopup.naver?movieCode='+str(num)
+    print(img_url)
+    res = requests.get(img_url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    
+    img = soup.find('img')
+    img_src = img['src']
+    urllib.request.urlretrieve(img_src, 'static/img/'+str(num)+'.jpg')
+
+def get_title(num):
+    movie_url = url+'movie/bi/mi/photoViewPopup.naver?movieCode='+str(num)
+    res = requests.get(movie_url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    
+    name = soup.find('img')
+    title = name['alt']
+    print(title)
+    return title
+    
+def get_movie_info(num):
+    movie_url = url+'movie/bi/mi/basic.naver?code='+str(num)
+    res = requests.get(movie_url)
+    
+    soup = BeautifulSoup(res.content, 'html.parser')
+    dom = etree.HTML(str(soup))
+
+    info = soup.find('div', class_='story_area')
+    info = soup.find('p', class_='con_tx')
+    
+    print(str(info).replace('<br/>', '\n').replace('<p class="con_tx">', '').replace('</p>', ''))
+    return str(info).replace('<br/>', '\n').replace('<p class="con_tx">', '').replace('</p>', '')
+   
+def get_movie_stat(num):
+    movie_url  = url+'movie/bi/mi/basic.naver?code='+str(num)
+    res = requests.get(movie_url)
+    
+    soup = BeautifulSoup(res.content, 'html.parser')
+    dom = etree.HTML(str(soup))
+    
+    info = soup.find('dl', class_='info_spec')
+    info = info.findAll('span')
+    
+    print(str(info[3].text).replace('\n', '').replace(' ', ''))
+
+    return str(info[3].text).replace('\n', '').replace(' ', '')
